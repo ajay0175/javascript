7585908c331ff1d69e4d6da2ac54944e0d17c18d
Ajay Nishad <najay9114@gmail.com>
2022-04-04 13:56:00 +0530

    Conditional Login

---
+++ js5Condtional_login.js
@@ -0,0 +1,9 @@
+// Allow user to login with email, facebook and google
+
+var email = true;
+var facebook = false;
+var google = false;
+
+if(email || facebook || google){
+    console.log("Login Success");
+}
