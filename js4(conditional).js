521a5fc2c8605eab23379aa6ea2daedc7e5a320d
Ajay Nishad <najay9114@gmail.com>
2022-04-04 13:50:21 +0530

    conditional operators

---
+++ js4(conditional).js
@@ -0,0 +1,16 @@
+//Conditional operator
+
+var temperature;
+
+temperature = 24;
+
+if(temperature === 20) {
+    console.log("It's cold outside");
+}
+
+if(temperature < 30) {
+    console.log("It's Moderate outside");
+}
+else if(temperature < 40) {
+    console.log("It's hot outside");
+}
