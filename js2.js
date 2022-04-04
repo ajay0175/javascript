4f3678a269ae469a9e27cfb0428f387f78c5e431
Ajay Nishad <najay9114@gmail.com>
2022-04-04 13:15:58 +0530

    Interpolation

---
+++ js2.js
@@ -0,0 +1,25 @@
+// our first USer Signup
+
+const uid = "abd123";
+
+// uid = "adfd23"; // these can't be changes
+
+var Full_name = "Ajaykumar Nishad";
+var email = "najay9114@gmail.com";
+var password = "1234";
+var confirm_Password = "1234";
+var course_count = 0;
+var is_logged_in_from_google = false;
+
+// console.log(Full_name);
+
+
+// `` => Interpolation
+console.log(`
+    userId : ${uid}
+    Name : ${Full_name}
+    email id : ${email}
+    password : ${password}
+
+
+`)
