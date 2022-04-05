8af91d48ac067322743a1694ffc160f60913e44b
Ajay Nishad <najay9114@gmail.com>
2022-04-05 20:08:13 +0530

    Context

---
+++ Intermediate/js10_context.js
@@ -0,0 +1,15 @@
+// sayhello();
+
+// function sayhello() {
+//     console.log("Hello");
+// }
+
+
+if (2 === 2) {
+    console.log("This is true");
+}
+
+var my_name = "Ajay";
+if (my_name === window.my_name) { // it run only in browser
+    console.log("This is true");
+}
