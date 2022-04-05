cca2a9b78b2d80602f3c7142425cd14903b520f2
Ajay Nishad <najay9114@gmail.com>
2022-04-05 19:28:37 +0530

    coercion and fasly values

---
+++ js7coercion_and_fasly_values.js
@@ -0,0 +1,15 @@
+// TODo : falsy
+// undefined
+// null
+// 0
+// ''
+// NaN
+
+var user = "2";
+
+if(2 === user){
+    console.log("Condtion is true");
+}
+
+// console.log(2 + 2);
+// console.log(2 + "2");
