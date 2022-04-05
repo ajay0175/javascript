59e7f6e17bf293b11cea98c1a7de55297c8f3812
Ajay Nishad <najay9114@gmail.com>
2022-04-05 19:41:09 +0530

    Function

---
+++ Intermediate/js8_function.js
@@ -0,0 +1,16 @@
+function say_hello(name){
+    console.log("Hello there, Ajay");
+    console.log(`Hello there, ${name}. How are you?`);
+}
+
+// say_hello("Ajay");
+// say_hello("abc");
+
+function namstey(){
+    return "hello in India"
+}
+
+var greeting = namstey();
+
+console.log(greeting);
+console.log(namstey());
